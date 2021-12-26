f99256a1911098da502c6a66ac22526943d1ff24
Louise Iyengar <louise.iyengar@flatironschool.com>
2021-12-25 22:40:34 -0500

    commit tree 5802d3fafd79e8773f5da7a6a8721bb71438d52b

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const num3 = 54;
+const num4 = 10;
+
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 5) + 1;
+const mod = num3 % num4;
+const max = Math.max(4,2,7, 20)
